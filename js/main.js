f19af83a1d2b9b5eae3949c922fbd085c4af5294
danil-boiko <boiko.81@list.ru>
2021-03-13 13:01:53 +0300

    create mobile version for header-bottom menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$(function () {
+/*$(function () {
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active')
     });
-});
+});*/
+
+document.getElementById('menu__btn').onclick = function() {
+    document.getElementById('menu__list').classList.add('menu__list--active');
+    document.getElementById('menu__btn').onclick = function() {
+        document.getElementById('menu__list').classList.toggle('menu__list--active');
+      }
+  }
