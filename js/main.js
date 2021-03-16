90560b9fbf9d3d8dc567c60fff7ed146c4633695
danil-boiko <boiko.81@list.ru>
2021-03-16 15:13:36 +0300

    change dib-menu to ul>li menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-/*$(function () {
+$(function () {
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active')
     });
-});*/
+});
 
-document.getElementById('menu__btn').onclick = function() {
-    document.getElementById('menu__list').classList.add('menu__list--active');
-    document.getElementById('menu__btn').onclick = function() {
-        document.getElementById('menu__list').classList.toggle('menu__list--active');
-      }
-  }
