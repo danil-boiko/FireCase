d5dc72c94b35b7b1767430a9a5acedaf2ff23dd4
danil-boiko <boiko.81@list.ru>
2021-03-20 22:05:42 +0300

    make modal window with tabs with bootstrap

--- js/main.js
+++ js/main.js
@@ -6,6 +6,45 @@ $(function () {
         $('.menu__mobile').toggleClass('menu__mobile--active')
     });
 
+    /*ЗАКРЫТИЕ ПРОШЛОГО ОКНА АККОРДЕОНА ПРИ ОТКРЫТИИ НОВОГО*/
 
+    /*$("#btn1").click(function () {
+        $("#collapseOne").classList.add('indsds');
+    });*/
+
+    document.getElementById('btn-1').onclick = function () {
+        document.getElementById('collapseOne').classList.add('in');
+        $("#collapseThree").collapse('hide');
+        $("#collapseTwo").collapse('hide');
+    }
+
+    document.getElementById('btn-2').onclick = function () {
+        document.getElementById('collapseTwo').classList.add('in');
+        $("#collapseOne").collapse('hide');
+        $("#collapseThree").collapse('hide');
+    }
+
+    document.getElementById('btn-3').onclick = function () {
+        document.getElementById('collapseThree').classList.add('in');
+        $("#collapseOne").collapse('hide');
+        $("#collapseTwo").collapse('hide');
+    }
+
+    $(".modal__points-item-1").click(function () {
+        $("#collapseOne").collapse('toggle');
+        $("#collapseThree").collapse('hide');
+        $("#collapseTwo").collapse('hide');
+    });
+    $(".modal__points-item-2").click(function () {
+        $('#collapseTwo').collapse('toggle');
+        $("#collapseOne").collapse('hide');
+        $("#collapseThree").collapse('hide');
+    });
+    $(".modal__points-item-3").click(function () {
+        $("#collapseThree").collapse('toggle');
+        $("#collapseOne").collapse('hide');
+        $("#collapseTwo").collapse('hide');
+    });
 });
+
 
