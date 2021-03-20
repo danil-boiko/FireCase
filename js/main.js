10f462ced0e8e2aa0cad75d3b32769c325710832
danil-boiko <boiko.81@list.ru>
2021-03-20 10:58:38 +0300

    delete js code and make modal window on bootstrap

--- js/main.js
+++ js/main.js
@@ -6,59 +6,6 @@ $(function () {
         $('.menu__mobile').toggleClass('menu__mobile--active')
     });
 
-    /*ОТКРЫТИЕ И ЗАКРЫТИЕ МОДАЛЬНЫХ ОКОН СО СТРАНИЦЫ ПРОДУКТА*/
 
-    $('.md-trigger-1').on('click', function () {
-        $('.md-modal-1').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
-
-    $('.md-trigger-2').on('click', function () {
-        $('.md-modal-2').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
-
-    $('.md-trigger-3').on('click', function () {
-        $('.md-modal-3').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
-
-    $('.md-close-1').on('click', function () {
-        $('.md-modal-1').removeClass('md-show');
-        document.body.style.overflow = 'visible';
-    });
-
-    $('.md-close-2').on('click', function () {
-        $('.md-modal-2').removeClass('md-show');
-        document.body.style.overflow = 'visible';
-    });
-
-    $('.md-close-3').on('click', function () {
-        $('.md-modal-3').removeClass('md-show');
-        document.body.style.overflow = 'visible';
-    });
-
-    /*ОТКРЫТИЕ НОВЫХ МОДАЛЬНЫХ ОКОН ИЗ УЖЕ ОТКРЫТЫХ МОДАЛЬНЫХ ОКОН*/
-
-    $('.md-reopen-1').on('click', function () {
-        $('.md-modal-2').removeClass('md-show');
-        $('.md-modal-3').removeClass('md-show');
-        $('.md-modal-1').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
-
-    $('.md-reopen-2').on('click', function () {
-        $('.md-modal-1').removeClass('md-show');
-        $('.md-modal-3').removeClass('md-show');
-        $('.md-modal-2').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
-
-    $('.md-reopen-3').on('click', function () {
-        $('.md-modal-1').removeClass('md-show');
-        $('.md-modal-2').removeClass('md-show');
-        $('.md-modal-3').addClass('md-show');
-        document.body.style.overflow = 'hidden';
-    });
 });
 
